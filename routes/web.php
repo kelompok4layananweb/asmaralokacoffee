b51ca59b337dcb247c9c88ad1352f300533387a3
zamzam <zamzam223313@gmail.com>
2024-06-18 18:35:23 +0700

    Menambahkan Route Halaman Frontend

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/menu', function () {
+    return view('menu');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/register', function () {
+    return view('register');
+});
